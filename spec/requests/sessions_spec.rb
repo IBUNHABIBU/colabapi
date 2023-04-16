7563c304e6fc00d77de94b1421fd8f4b578b2f73
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-16 23:15:32 +0300

    Test the login session

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /create" do
+  describe "POST /create" do
+    context "with valid credentials" do
+      let(:user) { create(:user) }
+      let(:params) do
+        {
+          email: user.email,
+          password: user.password
+        }
+      end
+
+    end
     it "returns http success" do
       get "/sessions/create"
       expect(response).to have_http_status(:success)
