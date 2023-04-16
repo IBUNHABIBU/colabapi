33d8ec269edac6aec95475a2fca3220c3281b2c6
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-17 00:07:16 +0300

    Define the model validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ApplicationRecord
+  has_one_attached :screenshot
+  validates :title, :github, :demo, :languages, presence: true
+
+  def imageUrl
+    if screenshot.present? && screenshot.attached?
+      Rails.application.routes.url_helpers.url_for(screenshot)
+    end
+  end
 end
