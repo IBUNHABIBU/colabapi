702ffcef8abe52d2757a53b00c199a8ebe5c774e
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-16 23:57:14 +0300

    Configure the url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,6 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
-
     allow do 
         # origins 'salum-microverse-final-capstone.netlify.app'
         origins 'http://localhost:3006'
