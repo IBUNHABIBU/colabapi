70ccf3b5aa4dfbc56819783463a2c549c67a80bc
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-16 23:00:29 +0300

    Define the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  resources :sessions, only: [:create]
+  resources :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
