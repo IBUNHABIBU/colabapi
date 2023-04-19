6dbb7ab20c2026f56dbdb953a165f3fae521d189
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-19 22:19:14 +0300

    Configure the capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 set :keep_releases, 5
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, ENV['BRANCH'] || 'dev'
+set :branch, ENV['BRANCH'] || 'main'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
