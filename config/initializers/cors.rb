8c6f8561af93fe0eb00fc673ba6bd8b9163c7901
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-19 13:21:17 +0300

    Add session store

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do 
         # origins 'salum-microverse-final-capstone.netlify.app'
-        origins 'http://localhost:3006'
+        origins 'http://localhost:5173'
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
