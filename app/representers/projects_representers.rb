7b97a0e243d301d1fd9334275c88ffabb879e7a2
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-19 03:29:39 +0300

    Add project representer

---
+++ app/representers/projects_representers.rb
@@ -0,0 +1,22 @@
+class ProjectsRepresenter
+    def initialize(projects)
+      @projects = projects
+    end
+  
+    def as_json
+      projects.map do |project|
+        {
+          id: project.id,
+          title: project.title,
+          github: project.github,
+          demo: project.demo,
+          languages: project.languages,
+          imageUrl: project.imageUrl
+        }
+      end
+    end
+  
+    private
+  
+    attr_reader :projects
+  end
