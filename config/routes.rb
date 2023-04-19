2570874ae52f40c133644008125d02e012f530bf
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-19 20:53:47 +0300

    Add skills routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do   
       resources :projects
-      resources :bookings
+      resources :skills
     end
   end
   # Defines the root path route ("/")
